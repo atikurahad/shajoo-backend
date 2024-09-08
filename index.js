7786689d74774cc9db63b74f5de338139ef23e0a
unknown <atikurahad@gmail.com>
2024-09-08 22:25:57 +0600

    database

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const app = require('./app')
+const mongoose =require('mongoose')
+
+
 
 app.listen(8080,()=>{
 console.log(`App is listening on port:8080`);
 
 })
+
+mongoose
+  .connect("mongodb://localhost/products")
+  .then(() => console.log("connected with DB"))
+  .catch((err) => console.log(err))
